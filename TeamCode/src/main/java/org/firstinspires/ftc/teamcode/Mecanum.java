80b91c205f9a019026971c474a3fa8e465cbf24c
Anton Dunbar <thedunbarboys@gmail.com>
2025-10-10 14:51:27 -0400

    Started Mecanum code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Mecanum {
+    public DcMotor leftFrontMotor;
+    public DcMotor rightFrontMotor;
+    public DcMotor leftBackMotor;
+    public DcMotor rightBackMotor;
+
+    public Mecanum (DcMotor leftFront, DcMotor rightFront,
+                    DcMotor leftBack, DcMotor rightBack) {
+        this.leftFrontMotor = leftFront;
+        this.rightFrontMotor = rightFront;
+        this.leftBackMotor = leftBack;
+        this.rightBackMotor = rightBack;
+    }
+
+    void drive(boolean forward, boolean strafe, boolean turn) {
+        // Put in the robot oriented driving here.
+    }
 }
