79e13a165b1e9f64c19584fd6270c4ac1b0ffcf2
Anton Dunbar <thedunbarboys@gmail.com>
2025-10-10 20:48:28 -0400

    Some basic drive code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisSubsystem.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class ChassisSubsystem {
+    public DcMotor leftFrontMotor;
+    public DcMotor rightFrontMotor;
+    public DcMotor leftBackMotor;
+    public DcMotor rightBackMotor;
+    private double currentSpeed = 0;
+
+    public ChassisSubsystem(DcMotor leftFront, DcMotor rightFront,
+                            DcMotor leftBack, DcMotor rightBack) {
+        this.leftFrontMotor = leftFront;
+        this.rightFrontMotor = rightFront;
+        this.leftBackMotor = leftBack;
+        this.rightBackMotor = rightBack;
+    }
+
+    void drive(double speed, double strafe, double turn) {
+        double rb = speed - turn + strafe;
+        double lb = speed + turn - strafe;
+        double rf = speed - turn - strafe;
+        double lf = speed + turn + strafe;
+
+        // If max is too big, we need to adjust
+        double max = Math.max(Math.abs(rf), Math.abs(rb));
+        max = Math.max(max,Math.abs(lf));
+        max = Math.max(max, Math.abs(lb));
+
+        if (max > 1.0) {
+            rb /= max; lb /= max; rf /= max; lf /= max;
+        }
+        leftFrontMotor.setPower(lf);
+        leftBackMotor.setPower(lb);
+        rightFrontMotor.setPower(rf);
+        rightBackMotor.setPower(rb);
+    }
+
+    // Probably want to have a slew rate function to avoid power overload
+    // I don't really know if that is needed
+    void slewRateDrive() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
-public class TeleOp {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp (name = "Basic TeleOp Program")
+public class TeleOp extends LinearOpMode {
+    public DcMotor leftFront;
+    public DcMotor rightFront;
+    public DcMotor leftBack;
+    public DcMotor rightBack;
+
+    public void runOpMode () {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBackMotor");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBackMotor");
+
+        ChassisSubsystem chassis = new ChassisSubsystem(leftFront, rightFront, leftBack, rightBack);
+
+    while (opModeIsActive()) {
+        chassis.drive(gamepad1.left_stick_y, gamepad1.left_stick_x,
+                                             gamepad1.right_stick_x);
+    }
+    }
 }
